4c816d85af462df17e0902287c8b9c095cb2c809
frstark <frstark@tsn.at>
2021-11-17 09:05:08 +0100

    Initial commit

---
+++ src/main/java/at/itkolleg/Temp_Test/TempTestApplication.java
@@ -0,0 +1,13 @@
+package at.itkolleg.Temp_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/itkolleg/Temp_Test/TempTestApplicationTests.java
@@ -0,0 +1,13 @@
+package at.itkolleg.Temp_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
